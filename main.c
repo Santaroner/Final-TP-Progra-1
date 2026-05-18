4cb45b1d2259710ee6873cacffbc10a9bce1dbb2
Rafael Santarone <84368192+Santaroner@users.noreply.github.com>
2026-05-17 22:51:37 -0300

    Initial commit

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <ctype.h>
+#include "pila.h"
+
+///probando git
+
+
+
+int main()
+{
+    printf("Hello world!\n");
+    return 0;
+}
